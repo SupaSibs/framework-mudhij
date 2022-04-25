76ff4a32a72f8bd8890ea066da662aa0d6ba74df
SupaSibs <92636078+SupaSibs@users.noreply.github.com>
2022-04-25 16:45:56 +0300

    Create toCustomComp.js

---
+++ src/toCustomComp.js
@@ -0,0 +1,42 @@
+function createType(name, type) {
+ return type 
+}
+
+function checkType(var, type) {
+if (typeof var !== type) {
+console.log("Types dont match");
+throw new Error("Type is wrong")
+}
+}
+
+function toComp(template, style, state, hooks, attrs) {
+checkType(template, "string")
+checkType(style, "object")
+checkType(state,"object")
+checkType(hooks,"object")
+checkType(attrs, "object")
+class Element extends HTMLElement {
+constructor() {super()}
+
+connectedCallBack() {
+this.innerHTML = template;
+for(let styles in style) }
+this.style[styles] = style[styles]
+}
+
+for (let states in state) {
+this.dataset[sta] = state[sta]
+}
+
+for (let hook in hooks) {
+this[hook] = hooks[hook]
+}
+
+for (let attr in attrs) {
+this[attr] = attrs[attr]
+}
+}
+}
+
+return Element
+}
