f58fec095f41e815cf0f30b209f4cc9888c0c433
SupaSibs <92636078+SupaSibs@users.noreply.github.com>
2022-04-27 22:28:52 -0700

    Create main-min.js

---
+++ main-min.js
@@ -0,0 +1,1 @@
+function toComp(t,e,s,n,o,i){class r extends HTMLElement{constructor(){super()}connectedCallback(){this.update=i,this.innerHTML=t;for(let t in e)this.style[t]=e[t];for(let t in s)this.dataset[sta]=s[sta];for(let t in n)this[t]=n[t];for(let t in o)this[t]=o[t]}}return r} function render(e,n){n.appendChild(e)}
